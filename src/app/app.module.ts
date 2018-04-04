cdf624a1868723eaac7f7adbe5228b4dac18a865
miyarin-tn <miyarin93@gmail.com>
2018-04-04 20:57:40 +0700

    Learn Angular 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+
+import { IpService } from './services/ip.service';
 
 
 import { AppComponent } from './app.component';
-import { WordComponent } from './word/word.component';
-import { PersonComponent } from './person/person.component';
-import { ListPersonComponent } from './list-person/list-person.component';
-import { ParentComponent } from './parent/parent.component';
-import { ChildComponent } from './child/child.component';
-import { ParentsComponent } from './parents/parents.component';
-import { ChildsComponent } from './childs/childs.component';
-import { CardComponent } from './card/card.component';
+import { WordComponent } from './components/word/word.component';
+import { PersonComponent } from './components/person/person.component';
+import { ListPersonComponent } from './components/list-person/list-person.component';
+import { ParentComponent } from './components/parent/parent.component';
+import { ChildComponent } from './components/child/child.component';
+import { ParentsComponent } from './components/parents/parents.component';
+import { ChildsComponent } from './components/childs/childs.component';
+import { CardComponent } from './components/card/card.component';
+import { IpComponent } from './components/ip/ip.component';
 
 
 @NgModule({
@@ -24,13 +29,15 @@ import { CardComponent } from './card/card.component';
     ChildComponent,
     ParentsComponent,
     ChildsComponent,
-    CardComponent
+    CardComponent,
+    IpComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [IpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/ip/ip.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { IpService } from '../../services/ip.service';
+
+@Component({
+  selector: 'app-ip',
+  templateUrl: './ip.component.html',
+  styleUrls: ['./ip.component.css']
+})
+export class IpComponent implements OnInit {
+
+  ip: String;
+
+  constructor(private ip_service: IpService) { }
+
+  ngOnInit() {
+    this.ip_service.get_ip()
+    .then(ip => this.ip = ip)
+    .catch(err => console.log(err));
+  }
+
+}
